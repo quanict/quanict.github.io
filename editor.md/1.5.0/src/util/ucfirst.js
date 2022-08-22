a7f9637da9fbbf993cd85172abf4acc91acc4bd6
quannh <quannh@runsystem.net>
2022-08-22 18:22:14 +0700

    update

--- editor.md/1.5.0/src/util/ucfirst.js
+++ editor.md/1.5.0/src/util/ucfirst.js
@@ -1,15 +1,15 @@
 /**
-    * 字符串首字母大写
-    * Only string first char to uppercase
-    * 
-    * @param   {String}    str            string
-    * @returns {String}                   string
-    */
-
-var firstUpperCase = function (str) {
+* 字符串首字母大写
+* Only string first char to uppercase
+* 
+* @param   {String}    str            string
+* @returns {String}                   string
+*/
+"use strict";
+let firstUpperCase = function (str) {
     return str.toLowerCase().replace(/\b(\w)/, function ($1) {
         return $1.toUpperCase();
     });
 };
 
-var ucfirst = firstUpperCase;
+let ucfirst = firstUpperCase;
